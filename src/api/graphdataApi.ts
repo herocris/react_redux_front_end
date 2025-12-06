f11e02f01abfb82d9ae664d337eb2dd0bbe58bb2
Cristhian Torres <cris_itg@yahoo.es>
2025-12-06 17:43:18 +0100

    Dejando comentada logica de guardado de token en localstorage

--- src/api/graphdataApi.ts
+++ src/api/graphdataApi.ts
@@ -26,4 +26,46 @@ calendarApi.interceptors.response.use(
 
 export default calendarApi;
 
+// import axios, {  } from 'axios';
+// import { getEnvVariables } from '../helpers/getEnvVariables';
 
+// const { VITE_API_URL } = getEnvVariables();
+
+// const calendarApi = axios.create({
+//     baseURL: VITE_API_URL,
+// });
+
+// // ✅ Interceptor para agregar token en cada request
+// calendarApi.interceptors.request.use((config) => {
+//     const token = typeof localStorage !== 'undefined'
+//         ? localStorage.getItem('token')
+//         : null;
+
+//     if (token) {
+//         config.headers.Authorization = `Bearer ${token}`;
+//     }
+
+//     return config;
+// });
+
+// // ✅ Interceptor para manejar errores globales
+// calendarApi.interceptors.response.use(
+//     (response) => response,
+//     (error) => {
+//         if (axios.isAxiosError(error)) {
+//             const status = error.response?.status;
+
+//             // 🔒 Si el token expiró o no es válido → Logout automático
+//             if (status === 401) {
+//                 localStorage.removeItem('token');
+//                 //window.location.href = '/login';
+//             }
+
+//             return Promise.reject(error.response?.data ?? error);
+//         }
+
+//         return Promise.reject(error);
+//     }
+// );
+
+// export default calendarApi;
